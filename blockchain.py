d5d4156a23cc12184ff16ee74ac8a225eedec9b3
Sebas <sebastiandelgadobruno@gmail.com>
2024-09-25 11:54:38 +0200

    kwargs

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ def nueva_funcion(monto_transaccion, ultima_transaccion=[1]):
     blockchain.append([ultima_transaccion, monto_transaccion])
 
 nueva_funcion(3)
-nueva_funcion(0.9, obtener_ultimo_valor_blockchain())
+nueva_funcion(ultima_transaccion=obtener_ultimo_valor_blockchain(), monto_transaccion=0.9)
 nueva_funcion(12.39, obtener_ultimo_valor_blockchain())
 
 print(blockchain)
