b613ed64401dbb49b5ccb022c2adb27c148bd49b
Sebas <sebastiandelgadobruno@gmail.com>
2024-09-25 11:50:01 +0200

    using default arguments

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,13 @@
-blockchain = [1]
+blockchain = []
 
 def obtener_ultimo_valor_blockchain():
     return blockchain [-1]
 
-def nueva_funcion(monto_transaccion):
-    blockchain.append([obtener_ultimo_valor_blockchain(), monto_transaccion])
+def nueva_funcion(monto_transaccion, ultima_transaccion=[1]):
+    blockchain.append([ultima_transaccion, monto_transaccion])
 
 nueva_funcion(3)
-nueva_funcion(0.9)
-nueva_funcion(12.39)
+nueva_funcion(0.9, obtener_ultimo_valor_blockchain())
+nueva_funcion(12.39, obtener_ultimo_valor_blockchain())
 
 print(blockchain)
