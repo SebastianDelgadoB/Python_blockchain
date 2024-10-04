de8868e8852eb87a6fbb13d969c431c8a70c968c
Sebas <sebastiandelgadobruno@gmail.com>
2024-10-04 12:42:45 +0200

    clean code DRY

--- blockchain.py
+++ blockchain.py
@@ -3,11 +3,20 @@ blockchain = []
 def obtener_ultimo_valor_blockchain():
     return blockchain [-1]
 
-def nueva_funcion(monto_transaccion, ultima_transaccion=[1]):
+def agregar_valor(monto_transaccion, ultima_transaccion=[1]):
     blockchain.append([ultima_transaccion, monto_transaccion])
 
-nueva_funcion(3)
-nueva_funcion(ultima_transaccion=obtener_ultimo_valor_blockchain(), monto_transaccion=0.9)
-nueva_funcion(12.39, obtener_ultimo_valor_blockchain())
+def obtener_input_usuario():
+    return float(input('El valor de su transaccion,por favor: '))
+
+
+valor_transaccion = obtener_input_usuario()
+agregar_valor(valor_transaccion)
+
+valor_transaccion = obtener_input_usuario()
+agregar_valor(ultima_transaccion=obtener_ultimo_valor_blockchain(), monto_transaccion=valor_transaccion)
+
+valor_transaccion = obtener_input_usuario()
+agregar_valor(valor_transaccion, obtener_ultimo_valor_blockchain())
 
 print(blockchain)
